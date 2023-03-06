9c5c8095246993b836ef97ea4c82aa6f8923f13b
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2023-03-06 11:02:01 -0300

    Padrão de projeto Factory
    Ajustes no proxy e criação de uma factory

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,29 +6,12 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        let self = this;
-        this._listaNegociacoes  = new Proxy(new ListaNegociacoes(), {
-
-            get(target, prop, receiver) {
-                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
-                    return function() {
-                        console.log(`Interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target, prop, receiver);
-            }
-        });
-        
-        //Passa a função que será executada QUANDO a lista de negociações for alterada
-        //this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
-        //passa a lista de negociações para a view
+        this._listaNegociacoes  = ProxyFactory.create(new ListaNegociacoes(), ['adiciona', 'esvazia'], model => this._negociacoesView.update(model));
     
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
         
-        this._mensagem = new Mensagem();
+        this._mensagem = ProxyFactory.create(new Mensagem(), ['texto'], model => this._mensagemView.update(model));
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
     }
