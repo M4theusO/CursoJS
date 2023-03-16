bff1a17fe9bccd6336a58ef493b47b2f52bcbb96
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2023-03-16 15:59:50 -0300

    Ajustes ao fechar connection

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,44 +1,66 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function () {
 
-class ConnectionFactory {
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
 
-    constructor() {
+    var connection = null;
 
-        throw new Error('Não é possível criar instâncias de ConnectionFactory');
+    var close = null;
+
+    return class ConnectionFactory {
+
+        constructor() {
+
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
+
+        static getConnection() {
+            
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName, version);
+
+                openRequest.onupgradeneeded = e => {
+
+                    ConnectionFactory._createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e => {
+                    if(!connection) {
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar diretamente a conexão');
+                        }
+                    }
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => {
+                    
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                };
+            });
+        }
+
+        static _createStores(connection) {
+
+            stores.forEach(store => {
+
+                if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+
+        static closeConnection() {
+            
+            if(connection) {
+                close();
+                connection = null;
+            }
+        }
     }
 
-    static getConnection() {
-        
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(dbName, version);
-
-            openRequest.onupgradeneeded = e => {
-
-                ConnectionFactory._createStores(e.target.result);
-            };
-
-            openRequest.onsuccess = e => {
-
-                resolve(e.target.result);
-            };
-
-            openRequest.onerror = e => {
-                
-                console.log(e.target.error);
-                reject(e.target.error.name);
-            };
-        });
-    }
-
-    static _createStores(connection) {
-
-        stores.forEach(store => {
-
-            if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
-    }
-}
+})(); //função autoinvocada
